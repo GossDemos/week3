2668908e3d336f1ccb36fe42e12cb414f771862e
Ilya Goss <goss@itmo.ru>
2025-02-02 19:58:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const { createReadStream } = require('fs');
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+};
+
+require ('http')
+.Server((req, res) => {
+  if (req.url === '/promise/') {
+    res.writeHead(200, '', CORS);
+    return res.end(`  function task(x){
+    return new Promise ((res, rej) => x < 18  ? res('yes') : rej('no'));
+  }`);
+  
+  }
+
+  if (req.url === '/login/') {
+    res.writeHead(200, '', CORS);
+    return res.end(`gossoudarev`);
+  }  
+
+  if (req.url === '/fetch/') {
+    res.writeHead(200, '', CORS);
+    return createReadStream(`fetch.html`).pipe(res);
+  } 
+  
+  
+})
+.listen();
